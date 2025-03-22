7e92eb0a689582eba40d5f7f60c98aef87ebd8e3
Khoi Nguyen <khoiit@Macbook-Pro-16.local>
2025-03-22 20:05:50 +0700

    push code

---
+++ src/test/java/com/example/cicd/spring_boot_cicd/CicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd.spring_boot_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
