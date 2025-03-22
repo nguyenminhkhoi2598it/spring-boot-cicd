a766781b54103ab16bce422f641e77e3a94fb7c0
Khoi Nguyen <khoiit@Macbook-Pro-16.local>
2025-03-22 20:13:45 +0700

    add controller

--- src/main/java/com/example/cicd/spring_boot_cicd/CicdDemoApplication.java
+++ src/main/java/com/example/cicd/spring_boot_cicd/CicdDemoApplication.java
@@ -2,9 +2,17 @@ package com.example.cicd.spring_boot_cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CicdDemoApplication {
+
+	@GetMapping("/welcome")
+	public String greeting() {
+		return "Hi, it's CI/CD";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicdDemoApplication.class, args);
